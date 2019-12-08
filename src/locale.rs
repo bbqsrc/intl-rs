c50de0f9e11e4d3dc36dd63b0997bd8362cc64c1
Brendan Molloy <brendan@bbqsrc.net>
2019-12-09 00:26:29 +0100

    Lazily handle posix locales

--- src/locale.rs
+++ src/locale.rs
@@ -8,35 +8,31 @@ use std::rc::Rc;
 
 #[derive(Debug, Clone)]
 pub struct Locale {
-    tag: LanguageTag
+    tag: LanguageTag,
 }
 
 #[cfg(windows)]
 mod platform {
-    use winapi::{um::winnls, ctypes::c_int};
     use std::ffi::OsString;
     use std::os::windows::ffi::OsStringExt;
+    use winapi::{ctypes::c_int, um::winnls};
 
     const MAX_LOCALE_NAME_LEN: usize = 85usize;
 
     #[inline]
     fn from_wide_string(vec: &[u16]) -> Result<String, OsString> {
-        let s = OsString::from_wide(&vec)
-            .into_string()?;
-        
+        let s = OsString::from_wide(&vec).into_string()?;
+
         Ok(s.split('\0').next().unwrap().to_owned())
     }
 
-    pub fn user_default_locale_name() -> Result<String, std::io::Error> {
+    pub fn locale_name() -> Result<String, std::io::Error> {
         let mut buf = vec![0u16; MAX_LOCALE_NAME_LEN];
 
         let ret = unsafe {
-            winnls::GetUserDefaultLocaleName(
-                buf.as_mut_ptr(),
-                MAX_LOCALE_NAME_LEN as c_int
-            )
+            winnls::GetUserDefaultLocaleName(buf.as_mut_ptr(), MAX_LOCALE_NAME_LEN as c_int)
         };
-        
+
         if ret == 0 {
             let err = std::io::Error::last_os_error();
             return Err(err);
@@ -52,17 +48,31 @@ mod platform {
     }
 }
 
+#[cfg(unix)]
+mod platform {
+    pub fn locale_name() -> Result<String, std::io::Error> {
+        let posix_tagish =
+            std::env::var("LANG").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+        let without_dangly_bits = posix_tagish.split(".").next().unwrap();
+        Ok(without_dangly_bits.replace("_", "-"))
+    }
+}
+
 thread_local! {
     pub static CURRENT_LOCALE: Rc<RefCell<Locale>> = Rc::new(RefCell::new(Locale::default()));
 }
 
 impl Default for Locale {
     fn default() -> Locale {
-        if let Ok(v) = platform::user_default_locale_name() {
-            return Locale { tag: v.parse().unwrap() };
+        if let Ok(v) = platform::locale_name() {
+            if let Ok(tag) = v.parse() {
+                return Locale { tag };
+            }
         }
 
-        Locale { tag: "und".parse().unwrap() }
+        Locale {
+            tag: "und".parse().unwrap(),
+        }
     }
 }
 
@@ -87,7 +97,7 @@ impl Locale {
     pub fn base_name(&self) -> Option<String> {
         let mut out = match self.tag.language.as_ref() {
             Some(v) => v.to_string(),
-            None => return None
+            None => return None,
         };
 
         if let Some(v) = self.script() {
@@ -159,7 +169,7 @@ mod tests {
 
         Locale::set_current(Locale::new("und-Latn-t-und-cyrl").unwrap());
         println!("{:?}", locale);
-        
+
         Locale::set_current(Locale::new("de-Latn-u-co-phonebk-ka-shifted-t-und-cyrl").unwrap());
         println!("{:?}", locale);
     }
